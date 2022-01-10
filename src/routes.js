acd2b29e89cb383d48ed27cdcb5d4792e1749960
Abel <abel291m@gmail.com>
2022-01-10 13:57:07 -0500

    changed case of dir

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,24 @@
 import AboutUs from "./pages/aboutUs/AboutUs"
-//import Blog from "./pages/blog/Blog"
-//import PostPage from "./pages/blog/PostPage"
+import Blog from "./pages/blog/Blog"
+import PostPage from "./pages/blog/PostPage"
 import CancellationPolicies from "./pages/cancellationPolicies/CancellationPolicies"
-import Contac from "./pages/cancellationPolicies/CancellationPolicies"
-import CookiePolicies from "./pages/cancellationPolicies/CancellationPolicies"
-import Gallery from "./pages/cancellationPolicies/CancellationPolicies"
-import Home from "./pages/cancellationPolicies/CancellationPolicies"
-import PrivacyPolicyPage from "./pages/cancellationPolicies/CancellationPolicies"
-import Reservation from "./pages/cancellationPolicies/CancellationPolicies"
-import Room from "./pages/cancellationPolicies/CancellationPolicies"
-import Rooms from "./pages/cancellationPolicies/CancellationPolicies"
-import TermsConditionsPage from "./pages/cancellationPolicies/CancellationPolicies"
+import Contac from "./pages/contact/Contact"
+import CookiePolicies from "./pages/cookiePolicies/CookiePolicies"
+import Gallery from "./pages/gallery/Gallery"
+import Home from "./pages/home/Home"
+import PrivacyPolicyPage from "./pages/privacyPolicies/PrivacyPolicies"
+import Reservation from "./pages/reservation/Reservation"
+import Room from "./pages/room/Room"
+import Rooms from "./pages/rooms/Rooms"
+import TermsConditionsPage from "./pages/termsConditions/TermsConditions"
 
 const routes = {
     home: { name: "home", path: "/", Component: Home },
     rooms: { name: "rooms", path: "/rooms", Component: Rooms },
     about: { name: "about-us", path: "/about-us", Component: AboutUs },
     contact: { name: "contact", path: "/contact", Component: Contac },
-    //blog: { name: "blog", path: "/blog", Component: Blog },
-    //post: { name: "post", path: "/post/:slug", Component: PostPage },
+    blog: { name: "blog", path: "/blog", Component: Blog },
+    post: { name: "post", path: "/post/:slug", Component: PostPage },
     gallery: { name: "gallery", path: "/gallery", Component: Gallery },
     room: { name: "room", path: "/room/:slug", Component: Room },
     termsConditions: { name: "terms_conditions", path: "/terms-conditions", Component: TermsConditionsPage },
