f150449fef7b23f11e7ef19eeb25043f2866c85f
Abel <abel291m@gmail.com>
2022-01-10 02:33:15 -0500

    vercel problem deploy 2

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 
 import AboutUs from "./pages/aboutUs/AboutUs"
 //import Blog from "./pages/blog/Blog"
-import PostPage from "./pages/blog/PostPage"
+//import PostPage from "./pages/blog/PostPage"
 import CancellationPolicies from "./pages/cancellationPolicies/CancellationPolicies"
 import Contac from "./pages/contact/Contact"
 import CookiePolicies from "./pages/cookiePolicies/CookiePolicies"
@@ -19,7 +19,7 @@ const routes = {
     about: { name: "about-us", path: "/about-us", Component: AboutUs },
     contact: { name: "contact", path: "/contact", Component: Contac },
     //blog: { name: "blog", path: "/blog", Component: Blog },
-    post: { name: "post", path: "/post/:slug", Component: PostPage },
+    //post: { name: "post", path: "/post/:slug", Component: PostPage },
     gallery: { name: "gallery", path: "/gallery", Component: Gallery },
     room: { name: "room", path: "/room/:slug", Component: Room },
     termsConditions: { name: "terms_conditions", path: "/terms-conditions", Component: TermsConditionsPage },
