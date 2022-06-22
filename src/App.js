b701586b61506ece021437383b5ab0dc3eeb07b5
Abel Morales <abel291m@gmail.com>
2022-06-22 12:43:10 -0500

    fix gallery img mazonry

--- src/App.js
+++ src/App.js
@@ -1,50 +1,37 @@
-import { Route, Switch, useLocation } from "react-router"
-import { SWRConfig } from "swr"
-import apiClient from "helpers/apiClient"
-import Footer from "components/Footer"
-import Header from "components/Header"
-
-import routes from "routes"
-import { useEffect, useState } from "react"
+import { Route, Switch } from "react-router";
+import { SWRConfig } from "swr";
+import apiClient from "helpers/apiClient";
+import Footer from "components/Footer";
+import Header from "components/Header";
+import routes from "routes";
 
 const App = () => {
-    const location = useLocation()
-    const [displayLocation, setDisplayLocation] = useState(location)
-    const [transitionStage, setTransistionStage] = useState("fadeIn")
-
-    useEffect(() => {
-        if (location !== displayLocation) setTransistionStage("fadeOut")
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [location])
     return (
         <>
             <SWRConfig
                 value={{
-                    fetcher: (url) => apiClient.get(url).then((res) => res.data),
+                    fetcher: (url) =>
+                        apiClient.get(url).then((res) => res.data),
                 }}
-            >   
-            <div className="">
-                <Header />
-                <div
-                    className={`${transitionStage}`}
-                    onAnimationEnd={() => {
-                        if (transitionStage === "fadeOut") {
-                            setTransistionStage("fadeIn")
-                            setDisplayLocation(location)
-                        }
-                    }}
-                >
-                    <Switch location={displayLocation}>
+            >
+                <div >
+                    <Header />
+
+                    <Switch>
                         {Object.keys(routes).map((key, index) => (
-                            <Route key={index} exact path={routes[key].path} component={routes[key].Component} />
+                            <Route
+                                key={index}
+                                exact
+                                path={routes[key].path}
+                                component={routes[key].Component}
+                            />
                         ))}
                     </Switch>
                 </div>
                 <Footer />
-                </div>
             </SWRConfig>
         </>
-    )
-}
+    );
+};
 
-export default App
+export default App;
--- src/pages/gallery/Gallery.js
+++ src/pages/gallery/Gallery.js
@@ -1,125 +1,109 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react";
+import FsLightbox from "fslightbox-react";
+import BannerTitle from "components/BannerTitle";
 
-import Isotope from "isotope-layout"
-import imagesLoaded from "imagesloaded"
-import FsLightbox from "fslightbox-react"
-import BannerTitle from "components/BannerTitle"
-
-import usePage from "hooks/usePage"
-import LoadingPage from "components/LoadingPage"
-import NotificationError from "components/NotificationError"
-import { formatErrors } from "helpers/helpers"
+import usePage from "hooks/usePage";
+import LoadingPage from "components/LoadingPage";
+import NotificationError from "components/NotificationError";
+import { formatErrors } from "helpers/helpers";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import { Transition } from "@headlessui/react";
 
 const Gallery = () => {
-    const { data, error } = usePage("page/gallery")
-    const textLoadedRef = useRef()
+    const { data, error } = usePage("page/gallery");
 
-    // useEffect(() => {
-    //     if (data) {
-    //         const galleryIso = document.getElementById("gallery-img")
-    //         //textLoadedRef.current.innerText = "Cargandooo..."
-    //         imagesLoaded(galleryIso, function () {
-    //             const iso = new Isotope(galleryIso, {
-    //                 percentPosition: true,
-    //                 itemSelector: ".img-item",
-    //                 horizontalOrder: true,
-    //             })
-    //             const btn_filter = document.querySelector(".filter-images")
-    //             if (btn_filter) {
-    //                 btn_filter.addEventListener("click", function (e) {
-    //                     if (!e.target.matches(".img-filter")) {
-    //                         return
-    //                     }
-    //                     let filter_data = e.target.getAttribute("data-filter")
-    //                     iso.arrange({ filter: filter_data })
-    //                     //refreshFsLightbox()
-    //                 })
-    //             }
-    //         }).on("done", function (instance) {
-    //            // textLoadedRef.current.innerText = ""
-    //         })
-    //     }
-    // }, [data])
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        if (data?.images) {
+            setImages(data.images);
+        }
+    }, [data]);
 
     const [lightboxController, setLightboxController] = useState({
-        toggler: true,
+        toggler: false,
         slide: 0,
-    })
+    });
 
-    const openLightboxOnSlide = (number) => {
+    function openLightboxOnSlide(number) {
         setLightboxController({
             toggler: !lightboxController.toggler,
             slide: number + 1,
-        })
+        });
     }
 
-    if (error) return <NotificationError errors={formatErrors(error)} />
-    if (!data) return <LoadingPage />
+    const handleButtonFilter = (slug) => {
+        let newImages = data.images.filter(
+            (item, key) => item.imageable.slug === slug
+        );
+        //console.log(newImages);
+        setImages(newImages);
+    };
+
+    if (error) return <NotificationError errors={formatErrors(error)} />;
+    if (!data) return <LoadingPage />;
 
     return (
         <>
-            <BannerTitle title={data.page.title} subTitle={data.page.sub_title} img={null} />
-            <div className="container mx-auto max-w-screen-xl space-y-8 pb-content">
-                <div>
-                    <p className="w-full md:w-2/4 text-gray-600">
-                        Hotel cartagena tiene mucho que ofrecer a los amantes de la naturaleza, los deportes, la historia, el ocio en la
-                        playa soleada y las aventuras activas.
-                    </p>
-                </div>
+            <BannerTitle
+                title={data.page.title}
+                subTitle={data.page.sub_title}
+                img={null}
+            />
+            <div className="container mx-auto space-y-8 pb-content ">
+                <p className="w-full md:w-2/4 text-gray-600">
+                    Hotel cartagena tiene mucho que ofrecer a los amantes de la
+                    naturaleza, los deportes, la historia, el ocio en la playa
+                    soleada y las aventuras activas.
+                </p>
 
-                <>
-                    {/* <div className="filter-images  flex flex-col sm:flex-row  text-lg space-x-0 sm:space-x-6">
-                        <button className="img-filter font-bold py-2 focus:outline-none capitalize" data-filter="*">
-                            Todas
+                <div className="flex flex-col sm:flex-row  text-lg space-x-0 sm:space-x-6">
+                    <button
+                        onClick={() => setImages(data.images)}
+                        className="img-filter font-bold py-2 focus:outline-none capitalize"
+                    >
+                        Todas
+                    </button>
+                    {data.galleries.map((gallery, index) => (
+                        <button
+                            onClick={() => handleButtonFilter(gallery.slug)}
+                            key={gallery.slug}
+                            className="font-bold py-2  focus:outline-none capitalize"
+                        >
+                            {gallery.slug}
                         </button>
-                        {data.galleries.map((gallery, index) => (
-                            <button
-                                key={index}
-                                data-filter={".data-" + gallery.slug}
-                                className="filter-images hover:cursor-pointer img-filter font-bold py-2  focus:outline-none capitalize"
-                            >
-                                {gallery.slug}
-                            </button>
-                        ))}
-                    </div> */}
-                    {/* <div>
-                        <div ref={textLoadedRef}></div>
-                        <div id="gallery-img">
-                            {data.images.map((image, index) => (
+                    ))}
+                </div>
+                <div className="relative">
+                    <ResponsiveMasonry
+                        columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 4 }}
+                    >
+                        <Masonry gutter="20px">
+                            {images.map((item, key) => (
                                 <div
-                                    key={index}
-                                    className={"img-item w-full md:w-2/4 lg:w-1/3 p-2 data-" + image.slug}
-                                    onClick={() => openLightboxOnSlide(index)}
+                                    key={key}
+                                    onClick={() => openLightboxOnSlide(key)}
+                                    className="cursor-pointer overflow-hidden"
                                 >
-                                    <div className="overflow-hidden">
-                                        <img src={image.image} className=" cursor-pointer rounded-md w-full" alt={image.image} />
-                                    </div>
+                                    <img
+                                        src={item.image}
+                                        alt={item.alt}
+                                        title={item.title}
+                                        className="w-full  rounded-md object-cover transform  duration-200 hover:scale-105"
+                                    />
                                 </div>
                             ))}
-                        </div>
-                    </div> */}
-                    {data.images.map((image, index) => (
-                                <div
-                                    key={index}
-                                    className={"img-item w-full md:w-2/4 lg:w-1/3 p-2 data-" + image.slug}
-                                    onClick={() => openLightboxOnSlide(index)}
-                                >
-                                    <div className="overflow-hidden">
-                                        <img src={image.image} className=" cursor-pointer rounded-md w-full" alt={image.image} />
-                                    </div>
-                                </div>
-                            ))}
-                    <div>           
-                    <FsLightbox
-                        toggler={lightboxController.toggler}
-                        sources={data.images.map((image) => image.image)}
-                        slide={lightboxController.slide}
-                    />
-                    </div>
-                </>
+                        </Masonry>
+                    </ResponsiveMasonry>
+                </div>
             </div>
+            <FsLightbox
+                toggler={lightboxController.toggler}
+                sources={images.map((image) => image.image)}
+                slide={lightboxController.slide}
+            />
         </>
-    )
-}
+    );
+};
 
-export default Gallery
+export default Gallery;
